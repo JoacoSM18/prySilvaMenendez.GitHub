c35aba45f04f73e5445adcae139e0eae24b111f4
Alumno <Alumno@PC42>
2025-08-27 12:13:14 -0300

    Agregar archivos de proyecto.

---
+++ prySilvaMenendez.GitHub/frmGitHub.cs
@@ -0,0 +1,10 @@
+namespace prySilvaMenendez.GitHub
+{
+    public partial class frmGitHub : Form
+    {
+        public frmGitHub()
+        {
+            InitializeComponent();
+        }
+    }
+}
