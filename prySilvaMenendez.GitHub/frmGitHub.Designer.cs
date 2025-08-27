0539d539777b22f15f864ca619bc1c26f855f5d0
JoacoSM18 <joako.sm.18@gmail.com>
2025-08-27 12:26:30 -0300

    Agregue Un Boton

--- prySilvaMenendez.GitHub/frmGitHub.Designer.cs
+++ prySilvaMenendez.GitHub/frmGitHub.Designer.cs
@@ -28,18 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnUno = new Button();
             SuspendLayout();
+            // 
+            // btnUno
+            // 
+            btnUno.Location = new Point(213, 90);
+            btnUno.Name = "btnUno";
+            btnUno.Size = new Size(102, 41);
+            btnUno.TabIndex = 0;
+            btnUno.Text = "Boton Uno";
+            btnUno.UseVisualStyleBackColor = true;
             // 
             // frmGitHub
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnUno);
             Name = "frmGitHub";
             Text = "Registro En GitHub de Mi Proyecto";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btnUno;
     }
 }
